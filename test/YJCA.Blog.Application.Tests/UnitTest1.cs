0fea6ae20ad085813956620c5ac291663cf74ef8
Gun_s <junjie_xmail@foxmail.com>
2022-12-30 10:09:18 +0800

    测试项目

---
+++ test/YJCA.Blog.Application.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace YJCA.Blog.Application.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
